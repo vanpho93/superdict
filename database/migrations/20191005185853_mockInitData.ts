dd108ff490360deca20e5f75c984f7ef897a6e73
Pho Nguyen <pho.nguyenvan@nashtechglobal.com>
2019-10-16 01:31:03 +0700

    fix

--- database/migrations/20191005185853_mockInitData.ts
+++ database/migrations/20191005185853_mockInitData.ts
@@ -6,20 +6,20 @@ export async function up(knex: Knex): Promise<any> {
 
   // init word type
   await knex('word_type').insert([
-    { word_type_id: 1, name: 'noun' },
-    { word_type_id: 2, name: 'verb' },
-    { word_type_id: 3, name: 'adjective' },
-    { word_type_id: 4, name: 'adverb' },
-    { word_type_id: 5, name: 'preposition' },
-    { word_type_id: 6, name: 'pronoun' },
-    { word_type_id: 7, name: 'determiner' },
-    { word_type_id: 8, name: 'conjunction' },
-    { word_type_id: 9, name: 'interjection' },
-    { word_type_id: 10, name: 'noun phrase' },
-    { word_type_id: 11, name: 'adverb phrase' },
-    { word_type_id: 12, name: 'adjective phrase' },
-    { word_type_id: 13, name: 'prepositional phrase' },
-    { word_type_id: 14, name: 'exclamation' },
+    { name: 'noun' },
+    { name: 'verb' },
+    { name: 'adjective' },
+    { name: 'adverb' },
+    { name: 'preposition' },
+    { name: 'pronoun' },
+    { name: 'determiner' },
+    { name: 'conjunction' },
+    { name: 'interjection' },
+    { name: 'noun phrase' },
+    { name: 'adverb phrase' },
+    { name: 'adjective phrase' },
+    { name: 'prepositional phrase' },
+    { name: 'exclamation' },
   ])
 
   // init lesson
