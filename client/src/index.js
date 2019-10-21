be525258717ebb35183b0956abee3809345ded4f
Pho Nguyen <pho.nguyenvan@nashtechglobal.com>
2019-10-21 21:11:22 +0700

    fix bug

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import { PersistGate } from 'redux-persist/integration/react'
 import Root from './Root';
 import * as serviceWorker from './serviceWorker';
 
-import { store } from './redux/store'
+import { store, persistor } from './redux/store'
 
-ReactDOM.render(<Provider store={store}><Root /></Provider>, document.getElementById('root'));
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Root />
+    </PersistGate>
+  </Provider>
+  , rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
