05f5aac27e2599f45099e508c8f2d0fbe2902858
Pho Nguyen <vanpho02@gmail.com>
2019-11-15 22:58:27 +0700

    Get lessons api (#14)

---
+++ server/src/models/lesson/get-lessons/tests/get-lessons.test.ts
@@ -0,0 +1,36 @@
+import { deepEqual } from 'assert'
+import request from 'supertest'
+import { app } from '../../../../app'
+import { TestUtilities, UserWithToken, Lesson, deepOmit } from '../../../../global-refs'
+
+const TEST_TITLE = 'GetLessons success'
+
+describe(TEST_TITLE, () => {
+  let user1: UserWithToken
+  let user2: UserWithToken
+
+  beforeEach(`${TEST_TITLE} prepare database`, async () => {
+    user1 = await TestUtilities.createUser('vanpho01@gmail.com')
+    user2 = await TestUtilities.createUser('vanpho02@gmail.com')
+    await Lesson.createMany([
+      { lessonId: 1, name: 'AAA', userId: user1.userId },
+      { lessonId: 2, name: 'BBB', userId: user1.userId },
+      { lessonId: 3, name: 'CCC', userId: user2.userId },
+    ])
+  })
+
+  it(`${TEST_TITLE} can get lessons`, async () => {
+    const response = await request(app).get('/api/lesson').set({ token: user1.token })
+    const expectedResponse = {
+      success: true,
+      result:
+        [{ lessonId: 1, name: 'AAA' }, { lessonId: 2, name: 'BBB' }],
+    }
+    deepEqual(deepOmit(response.body, ['created', 'userId', 'modified', 'created']), expectedResponse)
+  })
+
+  it(`${TEST_TITLE} cannot get without token`, async () => {
+    const response = await request(app).get('/api/lesson')
+    deepEqual(response.body, { success: false, message: 'MUST_BE_USER' })
+  })
+})
--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -2,9 +2,13 @@ import { IApiRoute } from '../metadata'
 // accounts
 import { loginRoute } from '../models/user/login/route'
 import { checkTokenRoute } from '../models/user/check-token/route'
+// vocabularies
 import { getVocabulariesRoute } from '../models/vocabulary/get-vocabulary/route'
 import { createVocabularysRoute } from '../models/vocabulary/create-vocabulary/route'
+// exam
 import { submitExamResultRoute } from '../models/exam/submit-exam-result/route'
+// lesson
+import { getLessonsRoute } from '../models/lesson/get-lessons/route'
 
 // tslint:disable-next-line: no-any
 export const routes: IApiRoute<any>[] = [
@@ -15,4 +19,5 @@ export const routes: IApiRoute<any>[] = [
   getVocabulariesRoute,
   createVocabularysRoute,
   submitExamResultRoute,
+  getLessonsRoute,
 ]
