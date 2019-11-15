50ffea4e8fcf7c3062d717915b1ecc93ccf443ad
Pho Nguyen <vanpho02@gmail.com>
2019-11-15 22:17:00 +0700

    Get more vocabularies for test (#13)

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,15 @@ import express from 'express'
 import { json } from 'body-parser'
 import path from 'path'
 import cors from 'cors'
+import qs from 'qs'
 
 import { routes } from './routes/routes'
 import { addRoute } from './routes/add-route'
 import { handleRouterError } from './routes/handle-router-error'
 
 export const app = express()
+
+app.set('query parser', (str: string) => qs.parse(str, { arrayLimit: Infinity }))
 
 app.use(json())
 app.use(cors())
