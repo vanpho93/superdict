aa9c9800ceab3fd292d07888aef0a9249a565bca
Pho Nguyen <pho.nguyenvan@nashtechglobal.com>
2019-11-16 14:15:06 +0700

    can get by lesson

--- client/src/app/services/fetch.service.ts
+++ client/src/app/services/fetch.service.ts
@@ -3,7 +3,8 @@ import { ajax } from 'rxjs/ajax'
 import { map } from 'rxjs/operators'
 import * as qs from 'qs'
 
-const ROOT_URL = 'https://superdict.herokuapp.com/api'
+// const ROOT_URL = 'https://superdict.herokuapp.com/api'
+const ROOT_URL = 'http://localhost:5000/api'
 
 @Injectable({ providedIn: 'root' })
 
