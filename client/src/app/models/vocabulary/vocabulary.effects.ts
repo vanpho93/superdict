331057ebca45baad40fbc4ade548aaa98aaf81bf
Pho Nguyen <pho.nguyenvan@nashtechglobal.com>
2019-11-16 19:21:20 +0700

    effect

--- client/src/app/models/vocabulary/vocabulary.effects.ts
+++ client/src/app/models/vocabulary/vocabulary.effects.ts
@@ -12,9 +12,11 @@ import {
   changeVocabularyDateRange,
   changeVocabularyPageSize,
   changeVocabularyLessonFilter,
+  sendAssginLessonRequest,
+  assginLessonSuccess,
 } from './vocabulary.action'
 import { IVocabulary, LessonFilter } from './vocabulary.metadata'
-import { Store } from '@ngrx/store'
+import { Store, select } from '@ngrx/store'
 import { State } from '..'
 
 interface IVocabularyQuery {
@@ -29,14 +31,14 @@ interface IVocabularyQuery {
 
 @Injectable()
 export class VocabularyEffects {
+  vocabularyState$ = this.store.select('vocabulary')
 
   fetch$ = createEffect(() => {
-    const vocabularyState$ = this.store.select('vocabulary')
     const filterByActionType = ofType(fetchVocabularies)
     const fetchVocabulary$ = this.actions$.pipe(filterByActionType, map(() => 1))
 
     return fetchVocabulary$.pipe(
-      withLatestFrom(vocabularyState$),
+      withLatestFrom(this.vocabularyState$),
       mergeMap(([_, { filter, paging }]) => this.fetchVocabularies({
           fromDate: filter.fromDate,
           toDate: filter.toDate,
@@ -62,6 +64,16 @@ export class VocabularyEffects {
     return this.actions$.pipe(filterByActionTypes, map(() => ({ type: '[Vocabulary Screen] Send fetch vocabularies request' })))
   })
 
+  assignLesson$ = createEffect(() => {
+    const sendAssignLesson$ = this.actions$.pipe(ofType(sendAssginLessonRequest))
+    const selectedVocabularyIds$ = this.vocabularyState$.pipe(select('selectedVocabularyIds')) as Observable<number[]>
+    return sendAssignLesson$.pipe(
+      withLatestFrom(selectedVocabularyIds$),
+      mergeMap(([{ lessonId }, vocabularyIds]) => this.assignLesson(lessonId, vocabularyIds)),
+      map(lessonId => assginLessonSuccess({ lessonId }))
+    )
+  }, { dispatch: false })
+
   constructor(
     private store: Store<State>,
     private actions$: Actions,
@@ -83,5 +95,12 @@ export class VocabularyEffects {
       lessonIds,
       isFindUnknownLesson: lesson === 'unknown',
     })
+  }
+
+  assignLesson(lessonId: number, vocabularyIds: number[]) {
+    return this.fetch.put('/vocabulary/assign-lesson', {
+      lessonId,
+      vocabularyIds,
+    }).pipe(map(() => lessonId))
   }
 }
